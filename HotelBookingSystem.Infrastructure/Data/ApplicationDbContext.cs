fc73c15b980c07afb8aa0ee4a45eece6388bef11
Luan Carosi <carosidev@gmail.com>
2025-02-12 15:31:07 -0300

    fix: correção da conexão com o banco de dados.

--- HotelBookingSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ HotelBookingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -9,6 +9,15 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<User> Users { get; set; }
     public DbSet<Hotel> Hotels { get; set; }
-    public DbSet<Room> Rooms { get; set; } 
+    public DbSet<Room> Rooms { get; set; }
     public DbSet<Reservation> Reservations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Room>()
+            .Property(r => r.PricePerNight)
+            .HasPrecision(18, 2);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
