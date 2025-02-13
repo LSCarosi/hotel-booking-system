6165e747685907d29f7e04462859f59e28744ed4
Luan Carosi <carosidev@gmail.com>
2025-02-13 10:51:26 -0300

    fix: remoção de arquivo HotelDTO.

--- HotelBookingSystem.Application/DTO/HotelDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HotelBookingSystem.Application.DTO;
-
-public class HotelDTO
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public int OwnerId { get; set; }
-}
