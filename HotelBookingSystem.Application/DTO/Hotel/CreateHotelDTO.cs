ebe404aaec449834bc2202d15e888baa95e5e6a0
Luan Carosi <carosidev@gmail.com>
2025-02-12 21:20:28 -0300

    feat: adiciona DTOs para gerenciamento de hotéis.

---
+++ HotelBookingSystem.Application/DTO/Hotel/CreateHotelDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelBookingSystem.Application.DTO.Hotel;
+
+public class CreateHotelDTO
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public int OwnerId { get; set; }
+}
---
+++ HotelBookingSystem.Application/DTO/Hotel/UpdateHotelDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelBookingSystem.Application.DTO.Hotel;
+
+public class UpdateHotelDTO
+{
+    public string? Name { get; set; }
+    public string? Location { get; set; }
+}
