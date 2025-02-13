4b3a49d0b9c0db6ced4cfdba439cc91dfdee82a7
Luan Carosi <carosidev@gmail.com>
2025-02-13 16:34:34 -0300

    fix: adição de Migrations para atualização das tabelas do banco de dados.

---
+++ HotelBookingSystem.Infrastructure/Migrations/20250213173917_AddIsAvailableToRoom.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsAvailableToRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Rooms");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Rooms");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
