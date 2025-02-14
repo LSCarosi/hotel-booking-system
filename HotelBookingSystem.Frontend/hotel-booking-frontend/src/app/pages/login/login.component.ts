4db6fa70662caa42533b16b035d2e75790d18c31
Luan Carosi <carosidev@gmail.com>
2025-02-13 21:38:04 -0300

    feat: iniciando frontend com angular, iniciado login e autenticação

---
+++ HotelBookingSystem.Frontend/hotel-booking-frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [FormsModule] 
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Login bem-sucedido!', response);
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/']);
+      },
+      error: () => {
+        this.errorMessage = 'Login falhou. Verifique suas credenciais!';
+      }
+    });
+  }
+}
---
+++ HotelBookingSystem.Frontend/hotel-booking-frontend/src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'https://localhost:7244/api/Auth/login'; 
+  private http = inject(HttpClient);
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { email, password });
+  }
+
+  logout(): void {
+    localStorage.removeItem('token'); 
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+}
