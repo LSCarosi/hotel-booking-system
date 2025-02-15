ddca0865482bd56a63c8466895200528dc77fed3
Luan Carosi <carosidev@gmail.com>
2025-02-15 09:18:39 -0300

    fix: adição de auth no routes.

--- HotelBookingSystem.Frontend/hotel-booking-frontend/src/app/app.routes.ts
+++ HotelBookingSystem.Frontend/hotel-booking-frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { authGuard } from './services/auth.guard'; 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
