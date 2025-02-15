04af064936e87c35fc559d13957ac61cd82a6a7c
Luan Carosi <carosidev@gmail.com>
2025-02-15 09:18:15 -0300

    feat: início na criação da tela de register e inclusão de validações.

--- HotelBookingSystem.Frontend/hotel-booking-frontend/src/app/pages/register/register.component.ts
+++ HotelBookingSystem.Frontend/hotel-booking-frontend/src/app/pages/register/register.component.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common'; // <-- ADICIONE ISSO AQUI
 
 @Component({
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule] // <-- ADICIONE CommonModule AQUI
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  role: number = 1;
+  
+  // Mensagens de erro individuais
+  nameError: string = '';
+  emailError: string = '';
+  passwordError: string = '';
+  confirmPasswordError: string = '';
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  onSubmit() {
+    console.log("Bateu aqui!");
+    if (!this.validateInputs()) return;
+
+    const userData = { name: this.name, email: this.email, password: this.password, role: this.role };
+
+    this.authService.register(userData).subscribe({
+      next: () => {
+        console.log('Conta criada com sucesso!');
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        alert('Erro ao criar conta. Tente novamente!');
+      }
+    });
+  }
+
+  validateInputs(): boolean {
+    let isValid = true;
+
+    if (this.name.length < 3) {
+      this.nameError = 'O nome deve ter pelo menos 3 caracteres.';
+      isValid = false;
+    } else {
+      this.nameError = '';
+    }
+
+    if (!this.validateEmail(this.email)) {
+      this.emailError = 'E-mail inválido.';
+      isValid = false;
+    } else {
+      this.emailError = '';
+    }
+
+    if (this.password.length < 6) {
+      this.passwordError = 'A senha deve ter pelo menos 6 caracteres.';
+      isValid = false;
+    } else {
+      this.passwordError = '';
+    }
+
+    if (this.password !== this.confirmPassword) {
+      this.confirmPasswordError = 'As senhas não coincidem!';
+      isValid = false;
+    } else {
+      this.confirmPasswordError = '';
+    }
+
+    return isValid;
+  }
+
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
 }
